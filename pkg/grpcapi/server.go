9f94023ffa751a52d0867c40b114dbaece90181e
manaraph <manaraph225@gmail.com>
2026-02-28 01:41:09 +0100

    Add grpcapi package

---
+++ pkg/grpcapi/server.go
@@ -0,0 +1,27 @@
+package grpcapi
+
+import (
+	"io"
+
+	streamv1 "github.com/manaraph/stream-aggregator/pkg/pb/stream/v1"
+	"github.com/manaraph/stream-aggregator/pkg/ws"
+)
+
+type Server struct {
+	streamv1.UnimplementedSensorServiceServer
+	Hub ws.Broadcaster
+}
+
+func (s *Server) StreamEvents(stream streamv1.SensorService_IngestSensorServer) error {
+	for {
+		e, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+		s.Hub.BroadcastEvent(e)
+	}
+}
