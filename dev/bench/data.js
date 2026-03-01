4edf00fed3732053130a20d9ff613d0215b6426b
github-action-benchmark <github@users.noreply.github.com>
2026-03-01 20:57:51 +0000

    add Benchmark (go) benchmark result for 4977cdd9c874ebaab45040881bd59124703742ff

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772398671961,
+  "repoUrl": "https://github.com/manaraph/stream-aggregator",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "manaraph225@gmail.com",
+            "name": "Manasseh Omachonu",
+            "username": "manaraph"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4977cdd9c874ebaab45040881bd59124703742ff",
+          "message": "Merge pull request #6 from manaraph/rf-structure-implementation\n\nLint protobuf file and update CI build",
+          "timestamp": "2026-03-01T21:56:05+01:00",
+          "tree_id": "8769853e01abf0a9da90b6274c375b8dc69ba3f3",
+          "url": "https://github.com/manaraph/stream-aggregator/commit/4977cdd9c874ebaab45040881bd59124703742ff"
+        },
+        "date": 1772398671078,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHubBroadcast",
+            "value": 777.7,
+            "unit": "ns/op\t     344 B/op\t       6 allocs/op",
+            "extra": "1749939 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkHubBroadcast - ns/op",
+            "value": 777.7,
+            "unit": "ns/op",
+            "extra": "1749939 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkHubBroadcast - B/op",
+            "value": 344,
+            "unit": "B/op",
+            "extra": "1749939 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkHubBroadcast - allocs/op",
+            "value": 6,
+            "unit": "allocs/op",
+            "extra": "1749939 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
